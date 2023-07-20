4d8554e99141ad358746c2eecd43e9902d89b16e
jamesdwamena1 <jamesdwamena1@gmail.com>
2023-07-20 12:56:01 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+  2 
+  3 /**
+  4 * main - tests function that prints if integer is positive or negati    ve
+  5 * Return: 0
+  6 */
+  7 
+  8 int main(void)
+  9 {
+ 10         int i;
+ 11 
+ 12         i = 0;
+ 13         positive_or_negative(i);
+ 14 
+ 15         return (0);
+ 16 }
