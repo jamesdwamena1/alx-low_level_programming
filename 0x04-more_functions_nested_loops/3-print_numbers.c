afff683f863ac23d0a6f96682dfca5429f4e41a5
jamesdwamena1 <jamesdwamena1@gmail.com>
2023-07-20 18:13:35 -0700

    nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,9 @@
 
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for (x  = 0; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
-  * print_most_numbers - Print the numbers since 0 to 9
-  * Description: vPrints the numbers excluding 2 and 4
-  * Return: the numbers since 0 to 9
-  */
+ * print_most_numbers - print num except 2 and 4
+ *
+ * return: void
+ */
 
 void print_most_numbers(void)
 {
-	int x = 0;
-	for (x <= 9; x++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-	if x (x == 2 || x ==4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
-}	
+}
